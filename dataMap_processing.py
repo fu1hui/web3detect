22b4b9feb4ab2f324f50add4f09ed0552b8fff46
Fu1Hui <fuyihui1028@gmail.com>
2024-11-17 00:57:18 +0800

    update

--- dataMap_processing.py
+++ dataMap_processing.py
@@ -5,23 +5,27 @@ attack_data_path = './attack_data'
 normal_data_path = './normal_data'
 
 
-def load_data_from_folder(folder_path, save_dir):
+#####################################
+## 保存文件还没有写，先把你的数据处理写上 ##
+#####################################
+
+def load_data_from_folder(folder_path, list1, list2):
     for filename in os.listdir(folder_path):
         if filename.endswith('.json'):
             with open(os.path.join(folder_path, filename), 'r') as f:
+                transaction_data = json.load(f)
                 if 'trace' in transaction_data and 'dataMap' in transaction_data['trace']:
-                    transaction_data = json.load(f)
-
                     fc_num = function_count(transaction_data)
                     gas = get_gas_value(transaction_data)
-
+                    if filename == '0x48164d3adbab78c2cb9876f6e17f88e321097fcd14cadd57556866e4ef3e185d.json' :
+                        print(fc_num, gas)  # 测试
+                    list1.append(fc_num)
+                    list2.append(gas)
 
 
 def function_count(transaction_data):
-    max_function_num = []
     function_dic = {}
 
-    # 检查 'trace' 和 'dataMap' 是否存在
     if 'trace' in transaction_data and 'dataMap' in transaction_data['trace']:
         data_map = transaction_data['trace']['dataMap']
 
@@ -30,18 +34,24 @@ def function_count(transaction_data):
             function = None
 
             if node_type == 0:
-                function = value.get('invocation', {}).get('decodedMethod', {}).get('name')
+                invocation = value.get('invocation')
+                if isinstance(invocation, dict):
+                    decoded_method = invocation.get('decodedMethod')
+                    if isinstance(decoded_method, dict):
+                        function = decoded_method.get('name')
             elif node_type == 1:
-                function = value.get('event', {}).get('decodedLog', {}).get('name')
+                event = value.get('event')
+                if isinstance(event, dict):
+                    decoded_log = event.get('decodedLog')
+                    if isinstance(decoded_log, dict):
+                        function = decoded_log.get('name')
 
             if function:
                 function_dic[function] = function_dic.get(function, 0) + 1
 
-        # 获取最大值并添加到列表中
-        max_value = max(function_dic.values(), default=-1)
-        max_function_num.append(max_value)
+    max_value = max(function_dic.values(), default=-1)
 
-    return max_function_num
+    return max_value
 
 
 def get_gas_value(transaction_data):
@@ -52,5 +62,10 @@ def get_gas_value(transaction_data):
     return -1
 
 
-load_data_from_folder(attack_data_path)
-load_data_from_folder(normal_data_path)
+normal_fc_num = []
+attack_fc_num = []
+normal_gas = []
+attack_gas = []
+load_data_from_folder(attack_data_path, attack_fc_num, attack_gas)
+load_data_from_folder(normal_data_path, normal_fc_num, normal_gas)
+print(attack_fc_num, '\n', normal_fc_num, '\n', attack_gas, '\n', normal_gas)
