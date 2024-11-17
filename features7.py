8ade994d030ec4d26b9c54864c895d78f0ccf3e5
Fu1Hui <fuyihui1028@gmail.com>
2024-11-17 11:42:20 +0800

    update

--- features7.py
+++ features7.py
@@ -67,7 +67,7 @@ def save_json(data, folder_name, json_filename):
     file_path = os.path.join(folder_name, json_filename)
 
     with open(file_path, 'w') as json_file:
-        json.dump(data, json_file, indent=4, ensure_ascii=False)  # indent=4 格式化输出
+        json.dump(data, json_file, indent=4, ensure_ascii=False)
 
 
 load_data_from_folder(attack_data_path, 'attack_json')
